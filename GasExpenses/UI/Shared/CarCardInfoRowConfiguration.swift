e6d16f76fc1b91e88eadc08b4dc9e27fa4737f71
Michal Brzezinski <sinner192@gmail.com>
2023-04-08 20:21:18 +0200

    Add `CarCardInfoRowConfiguration`

---
+++ GasExpenses/UI/Shared/CarCardInfoRowConfiguration.swift
@@ -0,0 +1,15 @@
+//
+//  CarCardInfoRowConfiguration.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 08/04/2023.
+//
+
+import Foundation
+
+struct CarCardInfoRowConfiguration: Hashable {
+    let iconName: String
+    let text: String
+    let helpText: String
+    let isPositive: Bool?
+}
