f34a1c5f3804e4121a0b3a7640eaddc02c3d2b08
Michal Brzezinski <sinner192@gmail.com>
2023-04-08 18:53:23 +0200

    Add `LandingPageView`

---
+++ GasExpenses/UI/LandingPageView.swift
@@ -0,0 +1,20 @@
+//
+//  LandingPageView.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 08/04/2023.
+//
+
+import SwiftUI
+
+struct LandingPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandingPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingPageView()
+    }
+}
