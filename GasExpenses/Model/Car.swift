e80ff6977eaf2f3e0dc49a35f48d9ea35e36ced4
Michal Brzezinski <sinner192@gmail.com>
2023-04-08 18:26:37 +0200

    Add `Car` model

---
+++ GasExpenses/Model/Car.swift
@@ -0,0 +1,15 @@
+//
+//  Car.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 08/04/2023.
+//
+
+import Foundation
+
+struct Car {
+    let name: String
+    let brand: String
+    let model: String
+    let refuels: (Date, Double, Double)
+}
