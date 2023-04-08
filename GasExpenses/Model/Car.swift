097ad7434393961e81393c9cd6648e36e70573cc
Michal Brzezinski <sinner192@gmail.com>
2023-04-08 18:51:26 +0200

    Add test and logic for average fuel consumption

--- GasExpenses/Model/Car.swift
+++ GasExpenses/Model/Car.swift
@@ -11,5 +11,17 @@ struct Car {
     let name: String
     let brand: String
     let model: String
-    let refuels: [(Date, Double, Double)]
+    let refuels: [(date: Date, mileage: Double, fuel: Double)]
+    
+    func averageFuelConsumptionSinceLast() -> Double {
+        let twoLastRefilsArray = refuels.suffix(2)
+        
+        guard twoLastRefilsArray.count == 2 else { return 0 }
+        
+        let lastRefill = twoLastRefilsArray[1]
+        let beforeLastRefill = twoLastRefilsArray[0]
+        let distanceDriven = lastRefill.mileage - beforeLastRefill.mileage
+        
+        return (lastRefill.fuel * 100) / distanceDriven
+    }
 }
--- GasExpensesTests/CarTests.swift
+++ GasExpensesTests/CarTests.swift
@@ -5,8 +5,25 @@
 //  Created by Michał Brzeziński on 08/04/2023.
 //
 
-import Foundation
+import XCTest
 
-final class CarTests {
+final class CarTests:XCTestCase {
     
+    func test_shouldCalculateAverageFuelConsumption() {
+        let model = makeSUT()
+        
+        let fuelConsumption = model.averageFuelConsumptionSinceLast()
+        
+        XCTAssertEqual(fuelConsumption, 7.0)
+    }
+    
+    func makeSUT() -> Car {
+        return Car(name: "My Subaru",
+                   brand: "Subaru",
+                   model: "Impreza",
+                   refuels: [
+                    (Date.now, 165000, 20),
+                    (Date.now, 165100, 7)
+                   ])
+    }
 }
