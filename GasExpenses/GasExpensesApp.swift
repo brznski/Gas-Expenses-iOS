650d87ff9ef6ade093a5e79c0db9a3f34036db03
Michal Brzezinski <mbz@bec.dk>
2023-04-08 18:13:03 +0200

    Initial Commit

---
+++ GasExpenses/GasExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasExpensesApp.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 08/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct GasExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
