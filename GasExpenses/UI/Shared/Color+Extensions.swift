15cc4bf1dc9a50e6c967f38f78fe0538297092e2
Michal Brzezinski <sinner192@gmail.com>
2023-04-08 19:09:42 +0200

    Add `Color+Extensions`

---
+++ GasExpenses/UI/Shared/Color+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extensions.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 08/04/2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let ui = Color.UI()
+
+    struct UI {
+        let background = Color("Background")
+        let contentOnBackground = Color("ContentOnBackground")
+    }
+}
+
+
