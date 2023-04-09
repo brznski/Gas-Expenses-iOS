5823c22a69cc131a87ae50c577a9e8cd5a6f1108
Michal Brzezinski <sinner192@gmail.com>
2023-04-09 23:37:25 +0200

    Change preview data in `CardWithTitleView`

--- GasExpensesTests/CarTests.swift
+++ GasExpensesTests/CarTests.swift
@@ -17,16 +17,29 @@ final class CarTests:XCTestCase {
         XCTAssertEqual(fuelConsumption, 7.0)
     }
     
-    
-    func test_shouldShowFormattedMileage() {
+    func test_shouldCalculateDifferenceSinceLast() {
         let model = makeSUT()
+        
+        let differenceSinceLast = model.distanceDifferenceSinceLast()
+        
+        XCTAssertEqual(differenceSinceLast, 100)
     }
+    
+    func test_shouldCalculateFuelDifferenceSinceLast() {
+        let model = makeSUT()
+        
+        let differenceSinceLast = model.fuelConsumptionDifferenceSinceLast()
+        
+        XCTAssertEqual(differenceSinceLast, -3)
+    }
+    
     func makeSUT() -> Car {
         return Car(name: "My Subaru",
                    brand: "Subaru",
                    model: "Impreza",
                    refuels: [
-                    (Date.now, 165000, 20),
+                    (Date.now, 164900, 20),
+                    (Date.now, 165000, 10),
                     (Date.now, 165100, 7)
                    ])
     }
