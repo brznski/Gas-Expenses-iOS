e51db33633005f99840f128a931d64a5a514adae
Michal Brzezinski <sinner192@gmail.com>
2023-04-10 18:13:33 +0200

    Add `HTTPMethod`

---
+++ GasExpenses/UI/Shared/Network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 10/04/2023.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+    case PUT
+    case DELETE
+}
