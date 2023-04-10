652923b91a13833b185bfd37d8c3d52e4e352367
Michal Brzezinski <sinner192@gmail.com>
2023-04-10 19:58:22 +0200

    Add Double extension for displaying mileage

---
+++ GasExpenses/UI/Shared/Extensions/Foundation/Double+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Extensions.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 10/04/2023.
+//
+
+import Foundation
+
+extension Double {
+    func odometerString() -> String? {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.groupingSeparator = ","
+        numberFormatter.decimalSeparator = "."
+        numberFormatter.groupingSize = 3
+        numberFormatter.maximumFractionDigits = 1
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.usesGroupingSeparator = true
+        
+        return numberFormatter.string(from: self as NSNumber)
+    }
+}
