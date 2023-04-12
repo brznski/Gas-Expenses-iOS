26d01435580030ebe148c0dab864a50a8dd7f07f
Michal Brzezinski <sinner192@gmail.com>
2023-04-12 18:43:03 +0200

    Add `CarOverviewView`.

---
+++ GasExpenses/Sources/UI/CarOverviewView.swift
@@ -0,0 +1,40 @@
+//
+//  CarOverviewView.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 12/04/2023.
+//
+
+import SwiftUI
+
+struct CarOverviewView: View {
+    @State var isSheetPresented = false
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                TitleAndIconHeaderView(title: "My cars") {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                        .onTapGesture {
+                            isSheetPresented.toggle()
+                        }
+                }
+
+                CarCardView(viewModel: .init(selectedCarDataStore: SelectedCarDataStore()))
+            }
+        }
+        .background(Color.ui.background)
+        .sheet(isPresented: $isSheetPresented, content: {
+            EmptyView()
+        })
+    }
+}
+
+struct CarOverviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarOverviewView()
+    }
+}
