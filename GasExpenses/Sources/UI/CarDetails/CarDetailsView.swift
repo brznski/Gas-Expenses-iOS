7265031db1de7e7f25ffc61e899d1b82562c8a27
Michal Brzezinski <sinner192@gmail.com>
2023-04-12 19:31:50 +0200

    Add `CarDetailsView`

---
+++ GasExpenses/Sources/UI/CarDetails/CarDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  CarDetailsView.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 12/04/2023.
+//
+
+import SwiftUI
+
+struct CarDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CarDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarDetailsView()
+    }
+}
