ed2a68951afd71452827c101c416155172cbb9d5
Michal Brzezinski <sinner192@gmail.com>
2023-04-13 21:57:09 +0200

    Add `CollapsableCardWithTitleView`

---
+++ GasExpenses/Sources/UI/Shared/CollapsableCardWithTitleView.swift
@@ -0,0 +1,67 @@
+//
+//  CollapsableCardWithTitleView.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 12/04/2023.
+//
+
+import SwiftUI
+
+struct CollapsableCardWithTitleView<Content: View>: View {
+    let title: LocalizedStringKey
+    let cardContent: (() -> Content)?
+    let alignment: HorizontalAlignment
+
+    @State var shouldShowContent = true
+
+    init(title: LocalizedStringKey, alignment: HorizontalAlignment = .center, cardContent: ( () -> Content)?) {
+        self.title = title
+        self.alignment = alignment
+        self.cardContent = cardContent
+    }
+
+    init(title: String, alignment: HorizontalAlignment = .center, cardContent: ( () -> Content)?) {
+        self.title = LocalizedStringKey(title)
+        self.alignment = alignment
+        self.cardContent = cardContent
+    }
+
+    var body: some View {
+        ZStack {
+            Color.ui.background
+                .ignoresSafeArea()
+            RoundedRectangle(cornerRadius: 8)
+                .padding()
+                .foregroundColor(.ui.contentOnBackground)
+            VStack(alignment: alignment) {
+                HStack {
+                    Text(title)
+                        .font(.title).bold()
+                        .padding()
+                    Spacer()
+                    Button {
+                        withAnimation {
+                            shouldShowContent.toggle()
+                        }
+                    } label: {
+                        Image(systemName: shouldShowContent ? "chevron.down" : "chevron.up")
+                    }
+                    .tint(Color.ui.action)
+                    .padding()
+
+                }
+                if shouldShowContent {
+                    cardContent?()
+                }
+            }.padding()
+        }
+    }
+}
+
+struct CollapsableCardWithTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CollapsableCardWithTitleView(title: "My car", cardContent: {
+            Text("Test")
+        })
+    }
+}
