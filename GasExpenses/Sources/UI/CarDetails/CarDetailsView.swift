fa2b89d4af2529deff5a82e53080359b42076585
Michal Brzezinski <sinner192@gmail.com>
2023-04-20 19:31:00 +0200

    Add basic charts to `CarDetailsView`

--- GasExpenses/Sources/UI/CarDetails/CarDetailsView.swift
+++ GasExpenses/Sources/UI/CarDetails/CarDetailsView.swift
@@ -6,15 +6,44 @@
 //
 
 import SwiftUI
+import Charts
 
 struct CarDetailsView: View {
+    let model: Car
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                CarCardView(viewModel: .init(carService: CarDataSource()))
+                CardWithTitleView(title: "Gas expenses") {
+                    Chart {
+                        ForEach(model.refuels) { refuel in
+                            BarMark(
+                                x: .value("Date", refuel.date),
+                                y: .value("", refuel.fuelAmount * refuel.costPerUnit)
+                            )
+                        }
+                    }
+                    .padding()
+                }
+                CardWithTitleView(title: "Mileage") {
+                    Chart {
+                        ForEach(model.refuels) { refuel in
+                            BarMark(
+                                x: .value("Date", refuel.date),
+                                y: .value("", refuel.mileage)
+                            )
+                        }
+                    }
+                    .padding()
+                }
+            }
+        }
+        .background(Color.ui.background)
     }
 }
 
 struct CarDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        CarDetailsView()
+        CarDetailsView(model: MockCarService().getFavouriteCar()!)
     }
 }
