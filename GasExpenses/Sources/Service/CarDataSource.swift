c3c058cf21c65f7ce2c887dbb2a6e360e918cb07
Michal Brzezinski <sinner192@gmail.com>
2023-04-24 23:21:32 +0200

    Add cache to `CarDataSource`
    If cache is available then return cached data
    otherwise fetch data from backend and
    cache it for future

--- GasExpenses/Sources/Service/CarDataSource.swift
+++ GasExpenses/Sources/Service/CarDataSource.swift
@@ -10,13 +10,19 @@ import Foundation
 final class CarDataSource {
     private let carService: CarServiceProtocol
     private var cars: [Car] = []
+    private let cache = Cache.shared
 
     init(carService: CarServiceProtocol) {
         self.carService = carService
     }
 
     func getCars() async throws -> [Car] {
+        if let cachedData = cache.getCacheEntry(key: "cars", object: [Car].self) {
+            return cachedData
+        }
+
         let cars = try await carService.getAllCars()
+        cache.cache(object: cars, key: "cars")
         return cars
     }
 
