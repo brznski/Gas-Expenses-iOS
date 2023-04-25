55e004dbf91b739cfc2c58f72cbff7e9a9e698fa
Michal Brzezinski <sinner192@gmail.com>
2023-04-25 21:00:09 +0200

    Delete force try in NetworkEngine

--- GasExpenses/Sources/Network/Shared/NetworkEngine.swift
+++ GasExpenses/Sources/Network/Shared/NetworkEngine.swift
@@ -19,9 +19,7 @@ struct NetworkEngine {
         }
 
         do {
-
-            // swiftlint:disable force_try
-            let decodedObject = try! JSONDecoder().decode(T.self, from: data)
+            let decodedObject = try? JSONDecoder().decode(T.self, from: data)
 
             return decodedObject
         } catch(let error) {
