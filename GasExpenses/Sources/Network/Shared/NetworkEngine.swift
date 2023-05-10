639e605216f20c26e256603c52930007d40a5ce7
Michal Brzezinski <sinner192@gmail.com>
2023-05-10 20:11:34 +0200

    Change HTTP Status handling and empty response for `NetworkEngine`

--- GasExpenses/Sources/Network/Shared/NetworkEngine.swift
+++ GasExpenses/Sources/Network/Shared/NetworkEngine.swift
@@ -9,14 +9,20 @@ import Foundation
 import Combine
 
 class NetworkEngine {
-    func sendRequest<T: Decodable>(endpoint: Endpoint, type: T.Type) async throws -> T {
+    func sendRequest<T: Decodable>(endpoint: Endpoint, type: T.Type?) async throws -> T {
         let (data, response) = try await URLSession.shared.data(for: endpoint.request)
 
         if let urlResponse = response as? HTTPURLResponse,
-           400...599 ~= urlResponse.statusCode {
+           201...599 ~= urlResponse.statusCode {
             switch urlResponse.statusCode {
-            case 300...400:
+            case 201:
+                if let emptyModel = EmptyModel() as? T {
+                    return emptyModel
+                }
+
                 throw NetworkEngineError.decodeError
+            case 300...400:
+                throw NetworkEngineError.other
             case 401, 403:
                 throw NetworkEngineError.unauthorized
             case 500...503:
@@ -24,6 +30,11 @@ class NetworkEngine {
             default:
                 throw NetworkEngineError.other
             }
+        }
+
+        if String(data: data, encoding: .utf8)!.isEmpty,
+           let emptyModel = EmptyModel() as? T {
+            return emptyModel
         }
 
         do {
