cc9782ac308041efb5b078326e64a1d83925360f
Michal Brzezinski <sinner192@gmail.com>
2023-05-11 17:07:01 +0200

    Add background color for `OnboardingView`, remove secondaryButtonConfig

--- GasExpenses/Sources/UI/Onboarding/OnboardingView.swift
+++ GasExpenses/Sources/UI/Onboarding/OnboardingView.swift
@@ -16,41 +16,39 @@ struct OnboardingView: View {
     let systemName: String
     let contentText: LocalizedStringKey
     let primaryButtonConfig: ButtonConfig
-    let secondaryButtonConfig: ButtonConfig
 
     @State private var scale = 0.6
 
     var body: some View {
         GeometryReader { geometry in
-            VStack(spacing: 80) {
-                Spacer()
-                Image(systemName: systemName)
-                    .resizable()
-                    .scaleEffect(scale)
-                    .animation(.easeInOut(duration: 0.5), value: scale)
-                    .scaledToFit()
-                    .frame(height: geometry.size.width * 0.4)
-                Text(contentText)
-                    .font(.title2)
-                    .multilineTextAlignment(.center)
-                VStack {
-                    ButtonPrimary {
-                        Text(primaryButtonConfig.title)
-                    } action: {
-                        primaryButtonConfig.onPress()
+            ZStack {
+                Color.ui.background
+                    .ignoresSafeArea()
+                VStack(spacing: 80) {
+                    Spacer()
+                    Image(systemName: systemName)
+                        .resizable()
+                        .scaleEffect(scale)
+                        .animation(.easeInOut(duration: 0.5), value: scale)
+                        .scaledToFit()
+                        .frame(height: geometry.size.width * 0.4)
+                    Text(contentText)
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                    VStack {
+                        ButtonPrimary {
+                            Text(primaryButtonConfig.title)
+                        } action: {
+                            primaryButtonConfig.onPress()
+                        }
                     }
-                    ButtonSecondary {
-                        Text(secondaryButtonConfig.title)
-                    } action: {
-                        secondaryButtonConfig.onPress()
-                    }
+                    Spacer()
                 }
-                Spacer()
+                .onAppear {
+                    scale = 1.0
+                }
+                .padding()
             }
-            .onAppear {
-                scale = 1.0
-            }
-            .padding()
         }
     }
 }
@@ -59,7 +57,6 @@ struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView(systemName: "lock.iphone",
                        contentText: "onboarding.account.text",
-                       primaryButtonConfig: ButtonConfig(title: "add.car") {},
-                       secondaryButtonConfig: ButtonConfig(title: "maybe.later") {})
+                       primaryButtonConfig: ButtonConfig(title: "add.car") {})
     }
 }
