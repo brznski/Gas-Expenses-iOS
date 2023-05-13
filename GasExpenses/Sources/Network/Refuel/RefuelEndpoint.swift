3f38a4ff45a066beedbe5333ee5470b218029944
Michal Brzezinski <sinner192@gmail.com>
2023-05-13 18:18:04 +0200

    Add endpoints for editing and deleting refuels

--- GasExpenses/Sources/Network/Refuel/RefuelEndpoint.swift
+++ GasExpenses/Sources/Network/Refuel/RefuelEndpoint.swift
@@ -44,3 +44,40 @@ struct AddRefuelEndpoint: BackendEndpoint {
     let refuel: Refuel
     let carID: Int
 }
+
+struct EditRefuelEndpoint: BackendEndpoint {
+    let refuel: Refuel
+    let carID: Int
+
+    var path: String = "refuel/edit"
+    var method: HTTPMethod = .PUT
+    var payload: [String: Any] {
+        [
+            "carID": "\(carID)",
+            "refuel": [
+                "id": refuel.id,
+                "title": refuel.title,
+                "comment": refuel.comment ?? nil,
+                "date": refuel.date,
+                "mileage": refuel.mileage,
+                "fuelAmount": refuel.fuelAmount,
+                "costPerUnit": refuel.costPerUnit,
+                "longitude": refuel.longitude ?? nil ,
+                "latitude": refuel.latitude ?? nil,
+                "documentBase64": refuel.documentBase64 ?? nil
+            ]
+        ]
+    }
+    var accessToken: String
+}
+
+struct DeleteRefuelEndpoint: BackendEndpoint {
+    let refuelID: Int
+
+    var path: String = "refuel/delete"
+    var method: HTTPMethod = .DELETE
+    var payload: [String: Any] {
+        ["refuelID": refuelID]
+    }
+    var accessToken: String
+}
