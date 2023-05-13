1837bb2ac347b81da858d3653c803062bdd835f0
Michal Brzezinski <sinner192@gmail.com>
2023-05-13 18:20:32 +0200

    Add endpoints for editing/deleting expenses

--- GasExpenses/Sources/Network/Expense/ExpenseEndpoint.swift
+++ GasExpenses/Sources/Network/Expense/ExpenseEndpoint.swift
@@ -40,3 +40,36 @@ struct AddExpenseEndpoint: BackendEndpoint {
     let carID: String
     let expense: Expense
 }
+
+struct EditExpenseEndpoint: BackendEndpoint {
+    var path: String = "expense/edit"
+    var method: HTTPMethod = .PUT
+    var payload: [String: Any] { [
+        "carID": carID,
+        "expense": [
+            "id": expense.id,
+            "amount": expense.amount,
+            "title": expense.title,
+            "date": expense.date,
+            "expenseType": expense.expenseType.rawValue,
+            "longitude": expense.longitude as Any,
+            "latitude": expense.latitude as Any,
+            "comment": expense.comment as Any,
+            "documentBase64": expense.documentBase64 as Any
+        ]
+    ]
+    }
+    var accessToken: String
+
+    let carID: String
+    let expense: Expense
+}
+
+struct DeleteExpenseEndpoint: BackendEndpoint {
+    let expenseID: Int
+
+    var path: String = "expense/delete"
+    var method: HTTPMethod = .DELETE
+    var payload: [String: Any] { ["expenseID": expenseID] }
+    var accessToken: String
+}
