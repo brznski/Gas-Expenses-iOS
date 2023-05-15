9dbe9b8eb74c8fa11df185add0b3a746dda26d2c
Michal Brzezinski <sinner192@gmail.com>
2023-05-15 11:48:37 +0200

    Add `CoreDataStack`

---
+++ GasExpenses/Sources/CoreData/CoreDataStack.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataStack.swift
+//  GasExpenses
+//
+//  Created by Michał Brzeziński on 14/05/2023.
+//
+
+import Foundation
+import CoreData
+
+final class CoreDataStack {
+    static let shared = CoreDataStack()
+    private init() {}
+
+    let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "GasExpenses")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
