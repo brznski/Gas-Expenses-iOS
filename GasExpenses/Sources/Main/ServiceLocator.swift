88cefd42bb0436ac54d275c3c197f668c526f8fa
Michal Brzezinski <sinner192@gmail.com>
2023-05-15 11:50:33 +0200

    Use `CoreDataStack` in `ServiceLocator`

--- GasExpenses/Sources/Main/ServiceLocator.swift
+++ GasExpenses/Sources/Main/ServiceLocator.swift
@@ -10,10 +10,12 @@ import Foundation
 final class ServiceLocator {
     static let shared = ServiceLocator()
     private let usesOnlineCommunication = Storage.usesOnlineServices
+    private let viewContext = CoreDataStack.shared.persistentContainer
     private init() {}
 
     func getCarService() -> CarServiceProtocol {
-        usesOnlineCommunication ? CarService() : CarService()
+//        usesOnlineCommunication ? CarService() : CarPersistentService(context: viewContext)
+        return CarPersistentService(context: viewContext.viewContext)
     }
 
     func getExpenseService() -> ExpenseServiceProtocol {
