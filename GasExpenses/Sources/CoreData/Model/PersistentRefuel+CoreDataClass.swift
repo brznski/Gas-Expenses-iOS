17698d4c0f2f8cf4fab7a9ecdc08def6390a7001
Michal Brzezinski <sinner192@gmail.com>
2023-05-16 13:56:23 +0200

    Add Refuel -> PersistentRefuel mapper

--- GasExpenses/Sources/CoreData/Model/PersistentRefuel+CoreDataClass.swift
+++ GasExpenses/Sources/CoreData/Model/PersistentRefuel+CoreDataClass.swift
@@ -13,3 +13,32 @@ import CoreData
 public class PersistentRefuel: NSManagedObject {
 
 }
+
+extension PersistentRefuel {
+    static func map(_ model: Refuel,
+                    context: NSManagedObjectContext,
+                    car: PersistentCar) -> PersistentRefuel {
+        let persistentModel = PersistentRefuel(context: context)
+
+        persistentModel.title = model.title
+        persistentModel.comment = model.comment
+        persistentModel.car = car
+        if let imageBase64 = model.documentBase64 {
+            persistentModel.image = Data(base64Encoded: imageBase64)
+        }
+        if let longitude = model.longitude {
+            persistentModel.longitude = longitude
+        }
+
+        if let latitude = model.latitude {
+            persistentModel.latitude = latitude
+        }
+
+        persistentModel.date = model.date.dateFromJSON()
+        persistentModel.fuelAmount = model.fuelAmount
+        persistentModel.mileage = model.mileage
+        persistentModel.costPerUnit = model.costPerUnit
+
+        return persistentModel
+    }
+}
