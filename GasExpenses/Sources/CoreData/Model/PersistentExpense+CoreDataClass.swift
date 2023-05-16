d4c34a0d0fefb246d1844584cb048895cecbbac2
Michal Brzezinski <sinner192@gmail.com>
2023-05-16 13:56:54 +0200

    Add PersistentExpense -> Expense mapper

--- GasExpenses/Sources/CoreData/Model/PersistentExpense+CoreDataClass.swift
+++ GasExpenses/Sources/CoreData/Model/PersistentExpense+CoreDataClass.swift
@@ -13,3 +13,31 @@ import CoreData
 public class PersistentExpense: NSManagedObject {
 
 }
+
+extension PersistentExpense {
+    static func map(_ model: Expense,
+                    context: NSManagedObjectContext,
+                    car: PersistentCar) -> PersistentExpense {
+        let persistentExpense = PersistentExpense(context: context)
+        persistentExpense.amount = model.amount
+        persistentExpense.title = model.title
+        persistentExpense.comment = model.comment
+        persistentExpense.date = model.date.dateFromJSON()
+        persistentExpense.expenseType = model.expenseType.rawValue
+
+        if let imageBase64 = model.documentBase64,
+           let imageData = Data(base64Encoded: imageBase64) {
+            persistentExpense.image = imageData
+        }
+
+        if let latitude = model.latitude {
+            persistentExpense.latitude = latitude
+        }
+
+        if let longitude = model.longitude {
+            persistentExpense.longitude = longitude
+        }
+
+        return persistentExpense
+    }
+}
